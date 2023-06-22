f845eb5b76c5cbb812ee0ebf597e7e109bdc3762
maxivega0 <maximilianovega600@gmail.com>
2023-06-21 23:44:52 -0300

    formato al documento

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div>
             <section className='bg-dark text-center text-light py-5'>
-            <p> &copy; Todos los derechos Reservados- MauricioSor</p>
+            <p> &copy; Todos los derechos Reservados- MauricioSor - MaximilianoVega</p>
             </section>
         </div>
     );
