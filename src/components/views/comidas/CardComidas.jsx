4a0a94a6d87a41a0f306f993a2e142e46a0992be
Mauricio Soria <mauricioutn2017@gmail.com>
2023-06-22 16:42:21 -0300

    bug de cards arreglado

--- src/components/views/comidas/CardComidas.jsx
+++ src/components/views/comidas/CardComidas.jsx
@@ -8,8 +8,8 @@ const CardComidas = ({ recetas }) => {
     return (
         <>
         <div><h1 className='text-start ms-2 mt-3'>Nuestras Selección Especial</h1></div>
-        <Container className='d-flex justify-content-center my-2'>
-        <Row xs={1} md={1} lg={3}xl={4}   >{
+        <Container >
+        <Row xs={1} md={1} lg={3}xl={4}  className='d-flex justify-content-center my-2' >{
             recetas.map((item, index) => (
                 <Card className='mx-1 my-2' style={{ width: '18rem' }} key={index}>
                     <Card.Img variant="top" className='my-1' src={item.imagen} style={{height:'180px'}} />
